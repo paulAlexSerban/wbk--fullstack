9dff1064fed4d9dc5f09cd630685107fe7d873ad
paulAlexSerban <paul.alex.serban@gmail.com>
2025-02-21 08:20:04 +0200

    feat: added basic test and lintint

---
+++ backend/apis/recipies-api/app/apps/calculator/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(a: int, b: int) -> int:
+    """Add x and y and return the result"""
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    """Subtract y from x and return the result"""
+    return a - b
--- backend/apis/recipies-api/scripts.bash
+++ backend/apis/recipies-api/scripts.bash
@@ -10,16 +10,25 @@ function up() {
     docker-compose up
 }
 
-function run-test() {
+function run-lint() {
     docker-compose run --rm app sh -c "flake8"
 }
 
-function migrate() {
+function run-autopep8() {
+    docker-compose run --rm app sh -c "autopep8 --in-place --aggressive --aggressive \$(find . -name '*.py' -not -path './venv/*')"
+}
+
+function run-migrate() {
     docker-compose run --rm app sh -c "python manage.py migrate"
+}
+
+function run-test() {
+    docker-compose run --rm app sh -c "python manage.py test"
 }
 
 # should be run only once to create the project
 # function start-django-project() {
 #     docker-compose run --rm app sh -c "django-admin startproject app ."
 # }
+
 $1
