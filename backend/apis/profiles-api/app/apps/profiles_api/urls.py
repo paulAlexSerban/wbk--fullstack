da3cd149a8a3937b3742a2f21f5c708ef22ea056
paulAlexSerban <paul.alex.serban@gmail.com>
2025-02-22 20:30:59 +0200

    feat: aded viewsets

--- backend/apis/profiles-api/app/apps/profiles_api/urls.py
+++ backend/apis/profiles-api/app/apps/profiles_api/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from apps.profiles_api import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+
+"""
+URL Patterns
+- define the URLs for the API
+- used with APIViews
+"""
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
+
