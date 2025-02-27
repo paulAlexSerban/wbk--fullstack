0f37d837c1fea0734a3409c4cfdd29c6653aae04
paulAlexSerban <paul.alex.serban@gmail.com>
2025-02-27 10:59:28 +0200

    feat: dynamodb populate scripts

--- workbook/04_cloud-infrastructure/projects/hello-nodejs-w-dynamodb/database/scripts/seed-scripts/dynamodb--td_notes.bash
+++ workbook/04_cloud-infrastructure/projects/hello-nodejs-w-dynamodb/database/scripts/seed-scripts/dynamodb--td_notes.bash
@@ -1,19 +1,19 @@
 #!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
 echo "Initializing some LocalStack resources..."
 
-LOREM_STRING="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+LOREM_STRING="lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum"
 TABLE_NAME="td_notes"
 REGION="us-east-1" # Change this to your AWS region
 
 generate_string() {
     local COUNT=$1
-    echo " [ info ] Generating a random string with $COUNT words..."
-    echo $LOREM_STRING | tr ' ' '\n' | shuf | head -n $COUNT | tr '\n' ' '
+    echo $LOREM_STRING | tr ' ' '\n' | shuf | head -n $COUNT | tr '\n' ' ' | xargs
 }
 
 generate_number() {
     local COUNT=$1
-    echo "[ info ] Generating a random number between 1 and $1..."
     echo $((RANDOM % $COUNT + 1))
 }
 
@@ -62,7 +62,7 @@ function add-item() {
     # Construct the JSON payload using jq
     JSON_PAYLOAD=$(jq -n \
         --arg user_id "$USER_ID" \
-        --argjson timestamp "$TIMESTAMP" \
+        --arg timestamp "$TIMESTAMP" \
         --arg title "$TITLE" \
         --arg cat "$CATEGORY" \
         --arg note_id "$NOTE_ID" \
@@ -85,6 +85,14 @@ function add-item() {
     echo "Item $COUNT inserted successfully!"
 }
 
+function add-item-from-json() {
+    local JSON_FILE="./json/item.json"
+    awslocal dynamodb put-item \
+        --table-name "$TABLE_NAME" \
+        --region "$REGION" \
+        --item file://$JSON_FILE
+}
+
 function populate_db() {
     local COUNT=$1
     for INDEX in $(seq 1 $COUNT); do
@@ -94,5 +102,125 @@ function populate_db() {
     echo "Finished populating $TABLE_NAME with $COUNT random items."
 }
 
+function describe-table() {
+    awslocal dynamodb describe-table --table-name $TABLE_NAME
+}
+
+function list-tables() {
+    awslocal dynamodb list-tables
+}
+
+function delete-table() {
+    awslocal dynamodb delete-table --table-name $TABLE_NAME
+}
+
+function update-item() {
+    # use update-item to update an existing item
+
+    KEY_JSON_PAYLOAD=$(jq -n \
+        --arg user_id "user_2" \
+        --arg timestamp "1740641114" \
+        '{
+        user_id: {S: $user_id},
+        timestamp: {N: ($timestamp | tostring)}
+    }')
+
+    EXPRESSION_ATTRIBUTE_NAMES_JSON_PAYLOAD=$(jq -n \
+        '{
+        "#content": "content"
+    }')
+
+    EXPRESSION_ATTRIBUTE_VALUES_JSON_PAYLOAD=$(jq -n \
+        --arg content "content_1_updated by me again" \
+        '{
+        ":content": {S: $content}
+    }')
+
+    # Run the AWS CLI command with the properly formatted JSON
+    awslocal dynamodb update-item \
+        --table-name "$TABLE_NAME" \
+        --region "$REGION" \
+        --key "$KEY_JSON_PAYLOAD" \
+        --update-expression "SET #content = :content" \
+        --expression-attribute-names "$EXPRESSION_ATTRIBUTE_NAMES_JSON_PAYLOAD" \
+        --expression-attribute-values "$EXPRESSION_ATTRIBUTE_VALUES_JSON_PAYLOAD"
+}
+
+function update-item-from-json() {
+    local KEY_FILE="./json/key.json"
+    local EXPRESSION_ATTRIBUTE_NAMES_FILE="./json/attribute-names.json"
+    local EXPRESSION_ATTRIBUTE_VALUES_FILE="./json/attribute-values.json"
+    awslocal dynamodb update-item \
+        --table-name "$TABLE_NAME" \
+        --region "$REGION" \
+        --key file://$KEY_FILE \
+        --update-expression "SET #var_title = :t" \
+        --expression-attribute-names file://$EXPRESSION_ATTRIBUTE_NAMES_FILE \
+        --expression-attribute-values file://$EXPRESSION_ATTRIBUTE_VALUES_FILE
+
+}
+
+function delete-item() {
+    awslocal dynamodb delete-item \
+        --table-name "$TABLE_NAME" \
+        --region "$REGION" \
+        --key '{
+        "user_id": {"S": "user_2"},
+        "timestamp": {"N": "1740641114"}
+    }'
+}
+
+function batch-write-from-json() {
+    local JSON_FILE="./json/batch-write.json"
+    # by not specificing the table name here, but in the file, we can use the same file for multiple tables
+    awslocal dynamodb batch-write-item \
+        --request-items file://$JSON_FILE
+}
+
+function get-item() {
+    awslocal dynamodb get-item \
+        --table-name "$TABLE_NAME" \
+        --region "$REGION" \
+        --key '{
+        "user_id": {"S": "user_3"},
+        "timestamp": {"N": "1740641116"}
+    }'
+}
+
+function query-items() {
+    EXPRESSION_ATTRIBUTE_NAMES='{
+        "#var_timestamp": "timestamp"
+    }'
+    EXPRESSION_ATTRIBUTE_VALUES='{
+        ":uid": {"S": "user_3"},
+        ":t": {"N": "5"}
+    }'
+    KEY_CONDITION_EXPRESSION="user_id = :uid AND #var_timestamp > :t"
+    awslocal dynamodb query \
+        --table-name "$TABLE_NAME" \
+        --region "$REGION" \
+        --key-condition-expression "$KEY_CONDITION_EXPRESSION" \
+        --expression-attribute-values "$EXPRESSION_ATTRIBUTE_VALUES" \
+        --expression-attribute-names "$EXPRESSION_ATTRIBUTE_NAMES"
+}
+
+function list-items-to-json() {
+    awslocal dynamodb scan \
+        --table-name "$TABLE_NAME" \
+        --region "$REGION" \
+        --output json >./json/export_items.json
+}
+
 # create-db
-populate_db 100
+populate_db 10
+
+# describe-table
+# list-tables
+# update-item
+# add-item-from-json
+# update-item-from-json
+# delete-item
+# batch-write-from-json
+# get-item
+# query-items
+# list-items-to-json
