405a700e7afa640ffec9dbd914f7f8ca1683aa73
paulAlexSerban <paul.alex.serban@gmail.com>
2025-04-06 15:03:56 +0300

    feat: hello strapi cms scripts

--- workbook/03_web-development/projects/hello-django/database/db-admin/pgAdmin/config_distro.py
+++ workbook/03_web-development/projects/hello-django/database/db-admin/pgAdmin/config_distro.py
@@ -15,7 +15,7 @@ SERVER_MODE = False
 # these are needed to make the container work with the environment variables
 SERVER_MODE = os.getenv('PGADMIN_CONFIG_SERVER_MODE').lower() in ('true', '1', 't')
 SERVER_NAME = os.getenv('PGADMIN_CONFIG_SERVER_NAME', 'pgAdmin4')
-DEFAULT_SERVER = os.getenv('PGADMIN_CONFIG_DEFAULT_SERVER', 'postgresql_database')
+DEFAULT_SERVER = os.getenv('PGADMIN_CONFIG_DEFAULT_SERVER', 'postgresql-database')
 DEFAULT_SERVER_PORT = int(os.getenv('PGADMIN_CONFIG_DEFAULT_SERVER_PORT', '5432'))
 DEFAULT_USERNAME = os.getenv('PGADMIN_CONFIG_DEFAULT_USERNAME', 'admin@admin.com')
 DEFAULT_PASSWORD = os.getenv('PGADMIN_CONFIG_DEFAULT_PASSWORD', 'changeme')
--- workbook/03_web-development/projects/hello-strapi-cms/readme.md
+++ workbook/03_web-development/projects/hello-strapi-cms/readme.md
@@ -4,3 +4,10 @@
 username: admin
 password: Password123
 email: admin@example.com
+
+## Encrypted Keys Zip - for Local Development Only
+password: password
+
+## Local Database PGAdmin
+username: admin
+password: password
