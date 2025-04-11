8a9832d71e470fe47b4fdb577c51945bfec3edd0
paulAlexSerban <paul.alex.serban@gmail.com>
2025-04-11 14:21:19 +0300

    feat: birthday buddy prod config

--- projects/birthday-buddy/frontend/apps/birthday-buddy-app/vite.config.ts
+++ projects/birthday-buddy/frontend/apps/birthday-buddy-app/vite.config.ts
@@ -1,42 +1,53 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 const PORT = parseInt(process.env.PORT) || 3000;
+const SERVICE_NAME: string = process.env.SERVICE_NAME;
+
+if (!SERVICE_NAME) {
+  throw new Error("SERVICE_NAME is not set");
+}
 
 if (isNaN(PORT)) {
-    throw new Error('Invalid PORT');
+  throw new Error("Invalid PORT");
 }
 
 export default defineConfig(() => {
-    const config = {
-        plugins: [react()],
-        base: '/',
-        server: {
-            port: PORT,
-        },
-        preview: {
-            port: PORT,
-        },
-        resolve: {
-            alias: {
-                '@': resolve(__dirname, './src'),
-            },
-        },
-        test: {
-            globals: true,
-            environment: 'jsdom',
-            setupFiles: './src/setupTests.ts',
-            // you might want to disable the `css: true` line, since we don't have
-            // tests that rely on CSS -- and parsing CSS is slow.
-            // I'm leaving it in here because often people want to parse CSS in tests.
-            css: true,
-            coverage: {
-                include: ['src/**/*.ts', 'src/**/*.tsx'],
-                exclude: ['src/**/*.test.ts', 'src/**/*.test.tsx', 'src/main.tsx', 'src/**/*.d.ts'],
-            },
-        },
-    };
+  const config = {
+    plugins: [react()],
+    base: "/",
+    server: {
+      port: PORT,
+      allowedHosts: [SERVICE_NAME],
+    },
+    preview: {
+      port: PORT,
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
+      // you might want to disable the `css: true` line, since we don't have
+      // tests that rely on CSS -- and parsing CSS is slow.
+      // I'm leaving it in here because often people want to parse CSS in tests.
+      css: true,
+      coverage: {
+        include: ["src/**/*.ts", "src/**/*.tsx"],
+        exclude: [
+          "src/**/*.test.ts",
+          "src/**/*.test.tsx",
+          "src/main.tsx",
+          "src/**/*.d.ts",
+        ],
+      },
+    },
+  };
 
-    return config;
+  return config;
 });
