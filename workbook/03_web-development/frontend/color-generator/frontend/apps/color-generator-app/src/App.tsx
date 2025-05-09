578647f0be81249affd98d17e83cd4844f1cf7fb
paulAlexSerban <paul.alex.serban@gmail.com>
2025-05-10 00:43:46 +0300

    feat(color-generator): init

---
+++ workbook/03_web-development/frontend/color-generator/frontend/apps/color-generator-app/src/App.tsx
@@ -0,0 +1,28 @@
+import ColorList from './components/ColorList';
+import Form from './components/Form';
+import Values from 'values.js';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+
+const App = () => {
+    const [colors, setColors] = useState(new Values('#f15025').all(10));
+
+    const addColor = (color: string) => {
+        try {
+            const newColors = new Values(color).all(10);
+            setColors(newColors);
+        } catch (err) {
+            const error = err as Error;
+            toast.error(error.message);
+        }
+    };
+
+    return (
+        <main>
+            <Form addColor={addColor} />
+            <ColorList colors={colors} />
+            <ToastContainer position="top-center" />
+        </main>
+    );
+};
+export default App;
