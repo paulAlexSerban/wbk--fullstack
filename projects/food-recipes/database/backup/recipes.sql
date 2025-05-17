b79ae7fe17e8918f6aafac2ca57f2d08ec3118ad
paulAlexSerban <paul.alex.serban@gmail.com>
2025-05-17 11:42:43 +0300

    feat(food-recipies): db update

--- projects/food-recipes/database/backup/recipes.sql
+++ projects/food-recipes/database/backup/recipes.sql
@@ -236,6 +236,113 @@ ALTER SEQUENCE public.auth_user_user_permissions_id_seq OWNED BY public.auth_use
 
 
 --
+-- Name: core_user; Type: TABLE; Schema: public; Owner: devuser
+--
+
+CREATE TABLE public.core_user (
+    id bigint NOT NULL,
+    password character varying(128) NOT NULL,
+    last_login timestamp with time zone,
+    is_superuser boolean NOT NULL,
+    email character varying(254) NOT NULL,
+    name character varying(255) NOT NULL,
+    is_active boolean NOT NULL,
+    is_staff boolean NOT NULL
+);
+
+
+ALTER TABLE public.core_user OWNER TO devuser;
+
+--
+-- Name: core_user_groups; Type: TABLE; Schema: public; Owner: devuser
+--
+
+CREATE TABLE public.core_user_groups (
+    id bigint NOT NULL,
+    user_id bigint NOT NULL,
+    group_id integer NOT NULL
+);
+
+
+ALTER TABLE public.core_user_groups OWNER TO devuser;
+
+--
+-- Name: core_user_groups_id_seq; Type: SEQUENCE; Schema: public; Owner: devuser
+--
+
+CREATE SEQUENCE public.core_user_groups_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.core_user_groups_id_seq OWNER TO devuser;
+
+--
+-- Name: core_user_groups_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: devuser
+--
+
+ALTER SEQUENCE public.core_user_groups_id_seq OWNED BY public.core_user_groups.id;
+
+
+--
+-- Name: core_user_id_seq; Type: SEQUENCE; Schema: public; Owner: devuser
+--
+
+CREATE SEQUENCE public.core_user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.core_user_id_seq OWNER TO devuser;
+
+--
+-- Name: core_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: devuser
+--
+
+ALTER SEQUENCE public.core_user_id_seq OWNED BY public.core_user.id;
+
+
+--
+-- Name: core_user_user_permissions; Type: TABLE; Schema: public; Owner: devuser
+--
+
+CREATE TABLE public.core_user_user_permissions (
+    id bigint NOT NULL,
+    user_id bigint NOT NULL,
+    permission_id integer NOT NULL
+);
+
+
+ALTER TABLE public.core_user_user_permissions OWNER TO devuser;
+
+--
+-- Name: core_user_user_permissions_id_seq; Type: SEQUENCE; Schema: public; Owner: devuser
+--
+
+CREATE SEQUENCE public.core_user_user_permissions_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.core_user_user_permissions_id_seq OWNER TO devuser;
+
+--
+-- Name: core_user_user_permissions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: devuser
+--
+
+ALTER SEQUENCE public.core_user_user_permissions_id_seq OWNED BY public.core_user_user_permissions.id;
+
+
+--
 -- Name: django_admin_log; Type: TABLE; Schema: public; Owner: devuser
 --
 
@@ -247,7 +354,7 @@ CREATE TABLE public.django_admin_log (
     action_flag smallint NOT NULL,
     change_message text NOT NULL,
     content_type_id integer,
-    user_id integer NOT NULL,
+    user_id bigint NOT NULL,
     CONSTRAINT django_admin_log_action_flag_check CHECK ((action_flag >= 0))
 );
 
@@ -402,6 +509,27 @@ ALTER TABLE ONLY public.auth_user_user_permissions ALTER COLUMN id SET DEFAULT n
 
 
 --
+-- Name: core_user id; Type: DEFAULT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user ALTER COLUMN id SET DEFAULT nextval('public.core_user_id_seq'::regclass);
+
+
+--
+-- Name: core_user_groups id; Type: DEFAULT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user_groups ALTER COLUMN id SET DEFAULT nextval('public.core_user_groups_id_seq'::regclass);
+
+
+--
+-- Name: core_user_user_permissions id; Type: DEFAULT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user_user_permissions ALTER COLUMN id SET DEFAULT nextval('public.core_user_user_permissions_id_seq'::regclass);
+
+
+--
 -- Name: django_admin_log id; Type: DEFAULT; Schema: public; Owner: devuser
 --
 
@@ -450,18 +578,18 @@ INSERT INTO public.auth_permission VALUES (9, 'Can add group', 3, 'add_group');
 INSERT INTO public.auth_permission VALUES (10, 'Can change group', 3, 'change_group');
 INSERT INTO public.auth_permission VALUES (11, 'Can delete group', 3, 'delete_group');
 INSERT INTO public.auth_permission VALUES (12, 'Can view group', 3, 'view_group');
-INSERT INTO public.auth_permission VALUES (13, 'Can add user', 4, 'add_user');
-INSERT INTO public.auth_permission VALUES (14, 'Can change user', 4, 'change_user');
-INSERT INTO public.auth_permission VALUES (15, 'Can delete user', 4, 'delete_user');
-INSERT INTO public.auth_permission VALUES (16, 'Can view user', 4, 'view_user');
-INSERT INTO public.auth_permission VALUES (17, 'Can add content type', 5, 'add_contenttype');
-INSERT INTO public.auth_permission VALUES (18, 'Can change content type', 5, 'change_contenttype');
-INSERT INTO public.auth_permission VALUES (19, 'Can delete content type', 5, 'delete_contenttype');
-INSERT INTO public.auth_permission VALUES (20, 'Can view content type', 5, 'view_contenttype');
-INSERT INTO public.auth_permission VALUES (21, 'Can add session', 6, 'add_session');
-INSERT INTO public.auth_permission VALUES (22, 'Can change session', 6, 'change_session');
-INSERT INTO public.auth_permission VALUES (23, 'Can delete session', 6, 'delete_session');
-INSERT INTO public.auth_permission VALUES (24, 'Can view session', 6, 'view_session');
+INSERT INTO public.auth_permission VALUES (13, 'Can add content type', 4, 'add_contenttype');
+INSERT INTO public.auth_permission VALUES (14, 'Can change content type', 4, 'change_contenttype');
+INSERT INTO public.auth_permission VALUES (15, 'Can delete content type', 4, 'delete_contenttype');
+INSERT INTO public.auth_permission VALUES (16, 'Can view content type', 4, 'view_contenttype');
+INSERT INTO public.auth_permission VALUES (17, 'Can add session', 5, 'add_session');
+INSERT INTO public.auth_permission VALUES (18, 'Can change session', 5, 'change_session');
+INSERT INTO public.auth_permission VALUES (19, 'Can delete session', 5, 'delete_session');
+INSERT INTO public.auth_permission VALUES (20, 'Can view session', 5, 'view_session');
+INSERT INTO public.auth_permission VALUES (21, 'Can add user', 6, 'add_user');
+INSERT INTO public.auth_permission VALUES (22, 'Can change user', 6, 'change_user');
+INSERT INTO public.auth_permission VALUES (23, 'Can delete user', 6, 'delete_user');
+INSERT INTO public.auth_permission VALUES (24, 'Can view user', 6, 'view_user');
 
 
 --
@@ -484,6 +612,25 @@ INSERT INTO public.auth_user VALUES (1, 'pbkdf2_sha256$260000$bpogjabLpVLNjXE2fr
 
 
 --
+-- Data for Name: core_user; Type: TABLE DATA; Schema: public; Owner: devuser
+--
+
+INSERT INTO public.core_user VALUES (1, 'pbkdf2_sha256$260000$XHaLr62bArp8HTcQaRA4ws$+EPjuF2qcX0d3b1+HFUcW/wwIDc+lwsApe1Y8dbAwj4=', '2025-05-17 08:40:01.483313+00', true, 'admin@example.com', '', true, true);
+
+
+--
+-- Data for Name: core_user_groups; Type: TABLE DATA; Schema: public; Owner: devuser
+--
+
+
+
+--
+-- Data for Name: core_user_user_permissions; Type: TABLE DATA; Schema: public; Owner: devuser
+--
+
+
+
+--
 -- Data for Name: django_admin_log; Type: TABLE DATA; Schema: public; Owner: devuser
 --
 
@@ -496,33 +643,34 @@ INSERT INTO public.auth_user VALUES (1, 'pbkdf2_sha256$260000$bpogjabLpVLNjXE2fr
 INSERT INTO public.django_content_type VALUES (1, 'admin', 'logentry');
 INSERT INTO public.django_content_type VALUES (2, 'auth', 'permission');
 INSERT INTO public.django_content_type VALUES (3, 'auth', 'group');
-INSERT INTO public.django_content_type VALUES (4, 'auth', 'user');
-INSERT INTO public.django_content_type VALUES (5, 'contenttypes', 'contenttype');
-INSERT INTO public.django_content_type VALUES (6, 'sessions', 'session');
+INSERT INTO public.django_content_type VALUES (4, 'contenttypes', 'contenttype');
+INSERT INTO public.django_content_type VALUES (5, 'sessions', 'session');
+INSERT INTO public.django_content_type VALUES (6, 'core', 'user');
 
 
 --
 -- Data for Name: django_migrations; Type: TABLE DATA; Schema: public; Owner: devuser
 --
 
-INSERT INTO public.django_migrations VALUES (1, 'contenttypes', '0001_initial', '2025-05-09 07:04:02.552471+00');
-INSERT INTO public.django_migrations VALUES (2, 'auth', '0001_initial', '2025-05-09 07:04:02.583639+00');
-INSERT INTO public.django_migrations VALUES (3, 'admin', '0001_initial', '2025-05-09 07:04:02.592382+00');
-INSERT INTO public.django_migrations VALUES (4, 'admin', '0002_logentry_remove_auto_add', '2025-05-09 07:04:02.594805+00');
-INSERT INTO public.django_migrations VALUES (5, 'admin', '0003_logentry_add_action_flag_choices', '2025-05-09 07:04:02.597028+00');
-INSERT INTO public.django_migrations VALUES (6, 'contenttypes', '0002_remove_content_type_name', '2025-05-09 07:04:02.602134+00');
-INSERT INTO public.django_migrations VALUES (7, 'auth', '0002_alter_permission_name_max_length', '2025-05-09 07:04:02.604638+00');
-INSERT INTO public.django_migrations VALUES (8, 'auth', '0003_alter_user_email_max_length', '2025-05-09 07:04:02.60706+00');
-INSERT INTO public.django_migrations VALUES (9, 'auth', '0004_alter_user_username_opts', '2025-05-09 07:04:02.609227+00');
-INSERT INTO public.django_migrations VALUES (10, 'auth', '0005_alter_user_last_login_null', '2025-05-09 07:04:02.611455+00');
-INSERT INTO public.django_migrations VALUES (11, 'auth', '0006_require_contenttypes_0002', '2025-05-09 07:04:02.612107+00');
-INSERT INTO public.django_migrations VALUES (12, 'auth', '0007_alter_validators_add_error_messages', '2025-05-09 07:04:02.6171+00');
-INSERT INTO public.django_migrations VALUES (13, 'auth', '0008_alter_user_username_max_length', '2025-05-09 07:04:02.62129+00');
-INSERT INTO public.django_migrations VALUES (14, 'auth', '0009_alter_user_last_name_max_length', '2025-05-09 07:04:02.624477+00');
-INSERT INTO public.django_migrations VALUES (15, 'auth', '0010_alter_group_name_max_length', '2025-05-09 07:04:02.634186+00');
-INSERT INTO public.django_migrations VALUES (16, 'auth', '0011_update_proxy_permissions', '2025-05-09 07:04:02.636639+00');
-INSERT INTO public.django_migrations VALUES (17, 'auth', '0012_alter_user_first_name_max_length', '2025-05-09 07:04:02.63892+00');
-INSERT INTO public.django_migrations VALUES (18, 'sessions', '0001_initial', '2025-05-09 07:04:02.644059+00');
+INSERT INTO public.django_migrations VALUES (1, 'contenttypes', '0001_initial', '2025-05-17 08:32:22.244875+00');
+INSERT INTO public.django_migrations VALUES (2, 'contenttypes', '0002_remove_content_type_name', '2025-05-17 08:32:22.248485+00');
+INSERT INTO public.django_migrations VALUES (3, 'auth', '0001_initial', '2025-05-17 08:32:22.269861+00');
+INSERT INTO public.django_migrations VALUES (4, 'auth', '0002_alter_permission_name_max_length', '2025-05-17 08:32:22.271872+00');
+INSERT INTO public.django_migrations VALUES (5, 'auth', '0003_alter_user_email_max_length', '2025-05-17 08:32:22.273895+00');
+INSERT INTO public.django_migrations VALUES (6, 'auth', '0004_alter_user_username_opts', '2025-05-17 08:32:22.275954+00');
+INSERT INTO public.django_migrations VALUES (7, 'auth', '0005_alter_user_last_login_null', '2025-05-17 08:32:22.279375+00');
+INSERT INTO public.django_migrations VALUES (8, 'auth', '0006_require_contenttypes_0002', '2025-05-17 08:32:22.281077+00');
+INSERT INTO public.django_migrations VALUES (9, 'auth', '0007_alter_validators_add_error_messages', '2025-05-17 08:32:22.282849+00');
+INSERT INTO public.django_migrations VALUES (10, 'auth', '0008_alter_user_username_max_length', '2025-05-17 08:32:22.285152+00');
+INSERT INTO public.django_migrations VALUES (11, 'auth', '0009_alter_user_last_name_max_length', '2025-05-17 08:32:22.286677+00');
+INSERT INTO public.django_migrations VALUES (12, 'auth', '0010_alter_group_name_max_length', '2025-05-17 08:32:22.289657+00');
+INSERT INTO public.django_migrations VALUES (13, 'auth', '0011_update_proxy_permissions', '2025-05-17 08:32:22.291653+00');
+INSERT INTO public.django_migrations VALUES (14, 'auth', '0012_alter_user_first_name_max_length', '2025-05-17 08:32:22.293415+00');
+INSERT INTO public.django_migrations VALUES (15, 'core', '0001_initial', '2025-05-17 08:32:22.312448+00');
+INSERT INTO public.django_migrations VALUES (16, 'admin', '0001_initial', '2025-05-17 08:32:22.320764+00');
+INSERT INTO public.django_migrations VALUES (17, 'admin', '0002_logentry_remove_auto_add', '2025-05-17 08:32:22.323078+00');
+INSERT INTO public.django_migrations VALUES (18, 'admin', '0003_logentry_add_action_flag_choices', '2025-05-17 08:32:22.32554+00');
+INSERT INTO public.django_migrations VALUES (19, 'sessions', '0001_initial', '2025-05-17 08:32:22.330654+00');
 
 
 --
@@ -530,6 +678,7 @@ INSERT INTO public.django_migrations VALUES (18, 'sessions', '0001_initial', '20
 --
 
 INSERT INTO public.django_session VALUES ('540hmgrq8ty3qeotmkk4k9u26rucb0em', '.eJxVjEEOwiAQRe_C2pDCgIBL9z0DGWZAqoYmpV0Z765NutDtf-_9l4i4rTVuPS9xYnERSpx-t4T0yG0HfMd2myXNbV2mJHdFHrTLceb8vB7u30HFXr-1LYMnhwF0ydaTVqZQZksJkicDygNwCAGM1_4MrnAZGA06i-RUKEm8P-hFOBg:1uDIFH:YZXFNneiprsnZDV1t7mJDi27s4zUnSyYFQHM-IjALP8', '2025-05-23 07:34:11.187635+00');
+INSERT INTO public.django_session VALUES ('859h57blkvml5g3nbyc60u6cunlz9ipz', '.eJxVjEEOwiAQAP_C2RCgQlmP3vsGsiyLVA1NSnsy_l1JetDrzGReIuC-lbA3XsOcxEVocfplEenBtYt0x3pbJC11W-coeyIP2-S0JH5ej_ZvULCVvnUmMWhvnT17YATW5CJk440ynJEGcpbYDJbyV1nw2ZsxgwNQalQs3h_ZvTeX:1uGD5N:E2fqfPEOIzD5qHAoNwWfbjQ1t8PxDRSFpLBsHmaq5oI', '2025-05-31 08:40:01.484313+00');
 
 
 --
@@ -572,6 +721,27 @@ SELECT pg_catalog.setval('public.auth_user_id_seq', 1, true);
 --
 
 SELECT pg_catalog.setval('public.auth_user_user_permissions_id_seq', 1, false);
+
+
+--
+-- Name: core_user_groups_id_seq; Type: SEQUENCE SET; Schema: public; Owner: devuser
+--
+
+SELECT pg_catalog.setval('public.core_user_groups_id_seq', 1, false);
+
+
+--
+-- Name: core_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: devuser
+--
+
+SELECT pg_catalog.setval('public.core_user_id_seq', 1, true);
+
+
+--
+-- Name: core_user_user_permissions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: devuser
+--
+
+SELECT pg_catalog.setval('public.core_user_user_permissions_id_seq', 1, false);
 
 
 --
@@ -692,6 +862,54 @@ ALTER TABLE ONLY public.auth_user
 
 
 --
+-- Name: core_user core_user_email_key; Type: CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user
+    ADD CONSTRAINT core_user_email_key UNIQUE (email);
+
+
+--
+-- Name: core_user_groups core_user_groups_pkey; Type: CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user_groups
+    ADD CONSTRAINT core_user_groups_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: core_user_groups core_user_groups_user_id_group_id_c82fcad1_uniq; Type: CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user_groups
+    ADD CONSTRAINT core_user_groups_user_id_group_id_c82fcad1_uniq UNIQUE (user_id, group_id);
+
+
+--
+-- Name: core_user core_user_pkey; Type: CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user
+    ADD CONSTRAINT core_user_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: core_user_user_permissions core_user_user_permissions_pkey; Type: CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user_user_permissions
+    ADD CONSTRAINT core_user_user_permissions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: core_user_user_permissions core_user_user_permissions_user_id_permission_id_73ea0daa_uniq; Type: CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user_user_permissions
+    ADD CONSTRAINT core_user_user_permissions_user_id_permission_id_73ea0daa_uniq UNIQUE (user_id, permission_id);
+
+
+--
 -- Name: django_admin_log django_admin_log_pkey; Type: CONSTRAINT; Schema: public; Owner: devuser
 --
 
@@ -795,6 +1013,41 @@ CREATE INDEX auth_user_username_6821ab7c_like ON public.auth_user USING btree (u
 
 
 --
+-- Name: core_user_email_92a71487_like; Type: INDEX; Schema: public; Owner: devuser
+--
+
+CREATE INDEX core_user_email_92a71487_like ON public.core_user USING btree (email varchar_pattern_ops);
+
+
+--
+-- Name: core_user_groups_group_id_fe8c697f; Type: INDEX; Schema: public; Owner: devuser
+--
+
+CREATE INDEX core_user_groups_group_id_fe8c697f ON public.core_user_groups USING btree (group_id);
+
+
+--
+-- Name: core_user_groups_user_id_70b4d9b8; Type: INDEX; Schema: public; Owner: devuser
+--
+
+CREATE INDEX core_user_groups_user_id_70b4d9b8 ON public.core_user_groups USING btree (user_id);
+
+
+--
+-- Name: core_user_user_permissions_permission_id_35ccf601; Type: INDEX; Schema: public; Owner: devuser
+--
+
+CREATE INDEX core_user_user_permissions_permission_id_35ccf601 ON public.core_user_user_permissions USING btree (permission_id);
+
+
+--
+-- Name: core_user_user_permissions_user_id_085123d3; Type: INDEX; Schema: public; Owner: devuser
+--
+
+CREATE INDEX core_user_user_permissions_user_id_085123d3 ON public.core_user_user_permissions USING btree (user_id);
+
+
+--
 -- Name: django_admin_log_content_type_id_c4bce8eb; Type: INDEX; Schema: public; Owner: devuser
 --
 
@@ -879,6 +1132,38 @@ ALTER TABLE ONLY public.auth_user_user_permissions
 
 
 --
+-- Name: core_user_groups core_user_groups_group_id_fe8c697f_fk_auth_group_id; Type: FK CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user_groups
+    ADD CONSTRAINT core_user_groups_group_id_fe8c697f_fk_auth_group_id FOREIGN KEY (group_id) REFERENCES public.auth_group(id) DEFERRABLE INITIALLY DEFERRED;
+
+
+--
+-- Name: core_user_groups core_user_groups_user_id_70b4d9b8_fk_core_user_id; Type: FK CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user_groups
+    ADD CONSTRAINT core_user_groups_user_id_70b4d9b8_fk_core_user_id FOREIGN KEY (user_id) REFERENCES public.core_user(id) DEFERRABLE INITIALLY DEFERRED;
+
+
+--
+-- Name: core_user_user_permissions core_user_user_permi_permission_id_35ccf601_fk_auth_perm; Type: FK CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user_user_permissions
+    ADD CONSTRAINT core_user_user_permi_permission_id_35ccf601_fk_auth_perm FOREIGN KEY (permission_id) REFERENCES public.auth_permission(id) DEFERRABLE INITIALLY DEFERRED;
+
+
+--
+-- Name: core_user_user_permissions core_user_user_permissions_user_id_085123d3_fk_core_user_id; Type: FK CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.core_user_user_permissions
+    ADD CONSTRAINT core_user_user_permissions_user_id_085123d3_fk_core_user_id FOREIGN KEY (user_id) REFERENCES public.core_user(id) DEFERRABLE INITIALLY DEFERRED;
+
+
+--
 -- Name: django_admin_log django_admin_log_content_type_id_c4bce8eb_fk_django_co; Type: FK CONSTRAINT; Schema: public; Owner: devuser
 --
 
@@ -892,6 +1177,14 @@ ALTER TABLE ONLY public.django_admin_log
 
 ALTER TABLE ONLY public.django_admin_log
     ADD CONSTRAINT django_admin_log_user_id_c564eba6_fk_auth_user_id FOREIGN KEY (user_id) REFERENCES public.auth_user(id) DEFERRABLE INITIALLY DEFERRED;
+
+
+--
+-- Name: django_admin_log django_admin_log_user_id_c564eba6_fk_core_user_id; Type: FK CONSTRAINT; Schema: public; Owner: devuser
+--
+
+ALTER TABLE ONLY public.django_admin_log
+    ADD CONSTRAINT django_admin_log_user_id_c564eba6_fk_core_user_id FOREIGN KEY (user_id) REFERENCES public.core_user(id) DEFERRABLE INITIALLY DEFERRED;
 
 
 --
