bf5f3714747ecc6fe08c42dd1e2738491b6be161
paulAlexSerban <paul.alex.serban@gmail.com>
2025-11-10 15:56:00 +0200

    feat: play with rendering props

--- projects/property-pulse-rentals/backend/services/ssr/property-pulse-rentals-ssr-service/app/properties/[id]/page.tsx
+++ projects/property-pulse-rentals/backend/services/ssr/property-pulse-rentals-ssr-service/app/properties/[id]/page.tsx
@@ -1,3 +1,9 @@
-const PropertyPage = () => {
-    return <div className="text-2xl">PropertyPage</div>
+
+const PropertyPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const { id } = await params
+    return <div>
+        <h1 className="text-2xl">PropertyPage</h1>
+        <div>Property ID: {id}</div>
+    </div>
 }
+export default PropertyPage
